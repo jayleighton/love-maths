fcecc8053f94ba2e5e1b4a684c6db773a803de8d
jayleighton <>
2023-11-28 22:45:04 +0000

    Add code to check the and answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 // Wait for DOM to finish loading before running the game
-
 // Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function() {
    let buttons = document.getElementsByTagName('button');
    for (let button of buttons){
         button.addEventListener('click', function() {
             if (this.getAttribute('data-type') === 'submit'){
-                alert('You clicked submit');
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute('data-type');
                 runGame(gameType);
@@ -35,12 +34,46 @@ function runGame(gameType) {
 
 }
 
+/**
+ * Check the answer against the first element in 
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
 
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert('Hey! You got it right! :D');
+
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`);
+
+    }
+
+    runGame(calculatedAnswer[1]);
+    
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer
+ */
 function calculateCorrectAnswer() {
+    // Get the values from the dom and convert to integer
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    // Get the operator from the dom
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === '+') {
+        return [operand1 + operand2, "addition"] 
+    } else {
+        alert(`Unimplemented operator: ${operator}`);
+        throw `Unimplemented operator: ${operator}. Aborting!`;
+    }
 
 }
 
